31fd1053746ff2f707847513cbbe2f12ff938038
Nikola Dobricic <nidza.dobricic@gmail.com>
2022-04-17 19:50:02 +0200

    Implemented Clamp, Min and Max template functions

--- Simulator/src/Utils.h
+++ Simulator/src/Utils.h
@@ -18,6 +18,37 @@ namespace utils
 		return false;
 	}
 
+	template<typename T>
+	T Clamp(T a, T min, T max)
+	{
+		assert(max >= min);
+
+		if (a < min)
+			return min;
+		if (a > max)
+			return max;
+
+		return a;
+	}
+
+	template<typename T>
+	T Min(T a, T b)
+	{
+		if (a <= b)
+			return a;
+
+		return b;
+	}
+
+	template<typename T>
+	T Max(T a, T b)
+	{
+		if (a >= b)
+			return a;
+
+		return b;
+	}
+
 	char* ReadFromPipe(HANDLE fileHandle, bool waitForInput);
 
 }
